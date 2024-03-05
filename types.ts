fd80885fa53187d0d3a4185985dc630c0bfe2fb6
EpicIbby101 <kevin_grittner@outlook.com>
2024-03-05 00:32:55 +0000

    Added products list to store frontend

--- types.ts
+++ types.ts
@@ -9,3 +9,25 @@ export interface Category {
   name: string;
   imageUrl: Billboard;
 }
+
+export interface Product {
+  id: string;
+  name: string;
+  category: string;
+  price: string;
+  quantity: Quantity;
+  isFeatured: boolean;
+  isArchived: boolean;
+  image: Image[];
+}
+
+export interface Image {
+  id: string;
+  url: string;
+}
+
+export interface Quantity {
+  id: string;
+  name: string;
+  value: string;
+}
